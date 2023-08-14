30fa8f5b426da2ac3398e0b7b098627cd451c63f
N/A <102998736+rrAsus@users.noreply.github.com>
2023-08-14 16:54:18 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2452,7 +2452,7 @@ Topbar.Hide.MouseButton1Click:Connect(function()
 	end
 end)
 	mouse.KeyDown:connect(function(key)
-		if (key:lower() == "LeftAlt") then
+		if (key:lower() == "leftalt") then
 		if Debounce then return end
 		if Hidden then
 			Hidden = false
