1b75057d67764e79bffd772622c8da00c9c9c72a
N/A <102998736+rrAsus@users.noreply.github.com>
2023-08-14 16:42:34 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -641,7 +641,7 @@ end
 
 function Hide()
 	Debounce = true
-	NSUILibrary:Notify({Title = "Interface Hidden", Content = "The interface has been hidden, you can unhide the interface by tapping RightShift", Duration = 7})
+	NSUILibrary:Notify({Title = "Interface Hidden", Content = "The interface has been hidden, you can unhide the interface by tapping RightAlt", Duration = 7})
 	TS:Create(Main, TweenInfo.new(0.5, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 470, 0, 400)}):Play()
 	TS:Create(Main.Topbar, TweenInfo.new(0.5, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 470, 0, 45)}):Play()
 	TS:Create(Main, TweenInfo.new(0.5, Enum.EasingStyle.Quint), {BackgroundTransparency = 1}):Play()
@@ -2021,9 +2021,8 @@ function NSUILibrary:CreateWindow(Settings)
 			end)
 
 			UserInputService.InputBegan:Connect(function(input, processed)
-
 				if CheckingForKey then
-					if input.KeyCode ~= Enum.KeyCode.Unknown and input.KeyCode ~= Enum.KeyCode.RightShift then
+					if (input.KeyCode ~= Enum.KeyCode.Unknown) and (input.KeyCode ~= Enum.KeyCode.RightAlt) then
 						local SplitMessage = string.split(tostring(input.KeyCode), ".")
 						local NewKeyNoEnum = SplitMessage[3]
 						Keybind.KeybindFrame.KeybindBox.Text = tostring(NewKeyNoEnum)
@@ -2451,7 +2450,7 @@ Topbar.Hide.MouseButton1Click:Connect(function()
 end)
 
 UserInputService.InputBegan:Connect(function(input, processed)
-	if (input.KeyCode == Enum.KeyCode.RightShift and not processed) then
+	if ((input.KeyCode == Enum.KeyCode.RightAlt) and not processed) then
 		if Debounce then return end
 		if Hidden then
 			Hidden = false
