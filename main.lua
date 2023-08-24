c82203b9602072d7ddb7d466574f966b1539d379
N/A <102998736+rrAsus@users.noreply.github.com>
2023-08-24 20:24:05 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1885,12 +1885,16 @@ function HDXLib:CreateWindow(Settings)
             Paragraph.Title.Text = ParagraphSettings.Title
             Paragraph.Content.Text = ParagraphSettings.Content
             Paragraph.Visible = true
-            Paragraph.Parent = TabPage
             Tab.Elements[ParagraphSettings.Title] = {
                 type = "paragraph",
                 section = ParagraphSettings.SectionParent,
                 element = Paragraph
             }
+			if SectionParent or ParagraphSettings.SectionParent.Holder then
+				Paragraph.Parent = SectionParent.Holder or ParagraphSettings.SectionParent.Holder
+            elseif SectionParent or ParagraphSettings.SectionParent.Holder == nil then
+				Paragraph.Parent = TabPage
+			end
             Paragraph.Content.Size = UDim2.new(0, 438, 0, Paragraph.Content.TextBounds.Y)
             --Paragraph.Content.Position = UDim2.new(0,465, 0,76)
             Paragraph.Size = UDim2.new(0,465, 0, Paragraph.Content.TextBounds.Y + 40)
