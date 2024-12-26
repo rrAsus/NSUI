92cc59674cd3584bab4c9b3ccd1375d238c39b31
Teremanphius <calcitriol1004@gmail.com>
2024-12-26 19:50:40 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1933,7 +1933,7 @@ NSUILib:ToggleOldTabStyle(Settings.OldTabLayout)
         function Tab:CreateSpacing(SectionParent, Size)
     local Spacing = Elements.Template.SectionSpacing:Clone()
     Spacing.Visible = true
-    Spacing.Size = UDim2.new(0, 385, 0, Size or 6)
+    Spacing.Size = UDim2.new(0, 390, 0, Size or 6)
     Spacing.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
     Spacing.BackgroundTransparency = 0
     Spacing.BorderSizePixel = 0
